787dfe121f77ac0ef2a076decd87d0eec50bebe5
Kim.McK <k.mckeever@alumni.unc.edu>
2019-06-17 21:52:20 -0400

    style update

--- Fast_Food/static/js/app2.js
+++ Fast_Food/static/js/app2.js
@@ -21,9 +21,16 @@ d3.csv("static/js/area_wide2_data.csv", function(data) {
  var x = d3.scaleLinear()
    .domain(d3.extent(data, function(d) { return d.region; }))
    .range([ 0, width ]);
+
+ // test x_axis
+ svg.pointsNames = ["Mountains", "Central", "Coastal"]
+ d3.svg.axis()
+       .scale(x).tickLabels(svg.pointsNames);
+   
  svg.append("g")
    .attr("transform", "translate(0," + height + ")")
    .call(d3.axisBottom(x).ticks(2));
+
 
  // Add Y axis
  var y = d3.scaleLinear()
@@ -35,7 +42,24 @@ d3.csv("static/js/area_wide2_data.csv", function(data) {
  // color palette
  var color = d3.scaleOrdinal()
    .domain(keys)
-   .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf'])
+   .range(['#FFD51E','#0066B3','#E51636','#D52B1E','#FEA91B','#005542','#343D98','#FCDDDB'])
+
+      // Legend is a work in progresssssss. JavaScript isn't my friend
+  // Handmade legend
+  var legendRectSize = 18;
+  var legendSpacing = 4;
+  var legend = svg.selectAll('.legend')
+  .data(color.domain())
+  .enter()
+  .append('g')
+  .attr('class', 'legend')
+  .attr('transform', function(d, i) {
+    var height = legendRectSize + legendSpacing;
+    var offset =  height * color.domain().length / 2;
+    var horz = -2 * legendRectSize;
+    var vert = i * height - offset;
+    return 'translate(' + horz + ',' + vert + ')';
+  });
 
  //stack the data?
  var stackedData = d3.stack()
